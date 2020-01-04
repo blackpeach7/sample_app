b20167bcee933a6214d1babe9d053b9b31bfe3a8
EC2 Default User <cococyi@yahoo.co.jp>
2020-01-04 09:56:30 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
    user = User.find_by(email: params[:session][:email].downcase)
    if user && user.authenticate(params[:session][:password])
-     log_in user
+     log_in usergit
      params[:session][:remember_me] == '1'?remember(user):forget(user)
      redirect_to user
    else
