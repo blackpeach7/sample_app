98053e07e1cac14bb948ae3344f727c988b78254
EC2 Default User <cococyi@yahoo.co.jp>
2020-01-04 09:44:38 +0000

    add remember me 9.23

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,7 @@ end
   end  
   
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end  
   
